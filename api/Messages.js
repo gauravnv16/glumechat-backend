cee3770a4cdf3aa747d051363db74e562d0c730d
gauravnv16 <114969570+gauravnv16@users.noreply.github.com>
2022-12-19 20:55:42 +0530

    ..

--- api/Messages.js
+++ api/Messages.js
@@ -1,13 +1,59 @@
 const express = require('express');
 const router = express.Router();
-const Messages = [];
+// const Messages = [];
+
+//intialize the body-parser
+const bodyParser = require('body-parser');
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+// import mongoose
+const mongoose = require('mongoose');
+mongoose.connect(`mongodb+srv://Gaurav_nv:Amirtha28@cluster0.83xxr.mongodb.net/glumeChat?retryWrites=true&w=majority`,() => {});
+
+
+// define message schema
+const messageSchema = new mongoose.Schema({
+    from: String,
+    to: String,
+    text: String,
+    time: String,
+});
+
+// create message model
+const Message = mongoose.model('Message', messageSchema);
+
+
+router.post('/sendMessage', (req, res) => {
+    //send message to the database
+    const message = new Message({
+        from: req.body.from,
+        to: req.body.to,
+        text: req.body.text,
+        time: req.body.time,
+    });
+    message.save((err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json({message:"message sent successfully"});
+        }
+    });
+});
 
 router.get('/', (req, res) => {
-    res.json(Messages);
+    //fetch the messages from the database
+    Message.find({}, (err, messages) => {
+        if (err) {
+        console.log(err);
+        } else {
+            res.json(messages);
+        }
+    }
+    )
 });
 
-router.post('/', (req, res) => {
-    Messages.push(req.query);
-    res.json(Messages);
-});
+router.get('/clear', (req, res) => {
+    Messages.length = 0;
+    res.json({message:"messages cleared successfully"});
+})
 module.exports = router;
--- api/User.js
+++ api/User.js
@@ -1,19 +1,88 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+
+//intialize the body-parser
+const bodyParser = require('body-parser');
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+
 const Users = [];
 
+// define user schema
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    id: String,
+    password: String,
+});
+
+// create user model
+
+
+mongoose.connect(`mongodb+srv://Gaurav_nv:Amirtha28@cluster0.83xxr.mongodb.net/glumeChat?retryWrites=true&w=majority`,() => {});
+const User = mongoose.model('User', userSchema);
+// import mongoose
+
+
+// router.get('/', (req, res) => {
+//     res.json(Users);
+// })
 router.get('/', (req, res) => {
-    res.json(Users);
+    //fetch the user from the database
+    User.find({}, (err, users) => {
+        if (err) {
+        console.log(err);
+        } else {
+            res.json(users);
+        }
+    })
 })
-router.post('/', (req, res) => {
-    const user = Users.find((user) => user.username === req.query.username);
+
+router.post('/login', (req, res) => {
+    console.log(req.body);
+    User.find({ email: req.body.email} , (err, user) => {
+        let user_resp = user[0];
+        if (err) {
+            res.json({message: "user not found"});
+        } else {
+            if (user_resp.password === req.body.password) {
+                const UObj = {
+                    name: user_resp.name,
+                    id: user_resp.id,
+                    lastmessage: user_resp.lastmessage,
+                    status: user_resp.status,
+                    time: user_resp.time,
+                }
+                res.json({message: "user logged in successfully",user:UObj});
+            } else {
+                res.json({message: "incorrect password"});
+            }
+        }
+    })
+})
+
+// register function
+router.post('/register', (req, res) => {
+    const user = Users.find((user) => user.username === req.body.username);
     if (user) {
         res.json({message:"user already exists with that name"});
     } else{
-        Users.push(req.query);
-        res.json(Users);
+        const newUser = new User({
+            name: req.body.name,
+            email: req.body.email,
+            id: req.body.id,
+            password: req.body.password,
+            lastmessage:"",
+            status:"",
+            time:"",
+        });
+        newUser.save();
+        res.json({message:"user registered successfully"});
     }
-    
+    // console.log(req.body);
+    // res.json({message:"user registered successfully"});
 })
 
 router.get('/clear', (req, res) => {
